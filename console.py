3aad8cd191bef69e53271255b7e6ba394999d8fd
cyber1ord <rooda43@gmail.com>
2023-07-13 22:26:20 +0000

    created the console file

---
+++ console.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Defines the HBnB console."""
+import cmd
+from models import storage
+from models.base_model import BaseModel
+
+
+def parse(arg):
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        if brackets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
+
+
+class HBNBCommand(cmd.Cmd):
+    """HBnB command interpreter.
+
+    Attributes:
+        prompt (str): The command prompt.
+    """
+
+    prompt = "(hbnb) "
+   
+    def do_quit(self, arg):
+        """Quit command to exit the program."""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF signal to exit the program."""
+        print("")
+        return True
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
