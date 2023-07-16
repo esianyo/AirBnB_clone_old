c2f19cd1665a267e00ab8c24c3308d57fee71f60
cyber1ord <rooda43@gmail.com>
2023-07-16 02:56:13 +0000

    correction

--- tests/test_console.py
+++ tests/test_console.py
@@ -41,7 +41,6 @@ class TestHBNB_prompt(unittest.TestCase):
 
 class TestHBNBcreate(unittest.TestCase):
 
-
     @classmethod
     def setUp(self):
         try:
@@ -109,8 +108,6 @@ class TestHBNBcreate(unittest.TestCase):
             self.assertLess(0, len(output.getvalue().strip()))
             testKey = "Review.{}".format(output.getvalue().strip())
             self.assertIn(testKey, storage.all().keys())
-
-
 
 
 if __name__ == "__main__":
